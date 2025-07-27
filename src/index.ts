d82e1144093f87187e720d6e7b55f70d0138cbfe
德二吹风机 <tasaedem@outlook.com>
2025-07-27 18:59:38 +0800

    feat: 添加 Random 指令

--- src/index.ts
+++ src/index.ts
@@ -64,15 +64,36 @@ async function getStatus(ctx: Context, session: any) {
   ctx.logger.info("Sent: "+msg)
   return msg;
 }
+async function getRandom(ctx: Context, session: any, min: number, max: number) {
+  ctx.logger.info("Got: "+session.text('.message', {
+    platform: session.platform,
+    selfId: session.selfId,
+  }));
+  const time = getHongKongTime();
+  let msg: string;
+  if (min==undefined || max==undefined) {
+    min=0
+    max=10000
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  msg = `${time}\n`+Math.floor(Math.random() * (max - min + 1)) + min;
+  ctx.logger.info("Sent: "+msg)
+  return msg;
+}
 
 // Main
 export function apply(ctx: Context) {
-  ctx.command('cx')
+  ctx.command('cx',"查询服务器当前人数。")
     .action(({ session }) => {
       return getServer(ctx,session);
     });
-  ctx.command('status')
+  ctx.command('status',"查询机器人状态。")
     .action(({ session }) => {
       return getStatus(ctx,session);
+    })
+  ctx.command('random [arg1:number] [arg2:number]',"随机数生成器，缺少参数时默认生成 0-10000 的随机数。")
+    .action(({ session },min,max) => {
+      return getRandom(ctx,session,min,max);
     })
 }
